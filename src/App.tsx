685a878d84c6ca512a40df39e279ae2fd9a23f78
otmjka <otmjka@gmail.com>
2025-04-11 19:38:04 +0300

    refactor: remove storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,5 @@
-import './App.css';
+import { buttonConfigs, buttonConfigsBottom } from './buttonConfigs';
 import { ConvertForm } from './features/ConvertForm';
-
-const buttonConfigs = [
-  {
-    id: 0,
-    progress: 0,
-    label: '25%',
-  },
-  {
-    id: 1,
-    progress: 0,
-    label: '50%',
-  },
-  {
-    id: 2,
-    progress: 0,
-    label: '75%',
-  },
-  {
-    id: 3,
-    progress: 0,
-    label: '100%',
-  },
-];
-
-const buttonConfigsBottom = [
-  {
-    id: 0,
-    progress: 100,
-    label: '25%',
-  },
-  {
-    id: 1,
-    progress: 0,
-    label: '50%',
-  },
-  {
-    id: 2,
-    progress: 0,
-    label: '75%',
-  },
-  {
-    id: 3,
-    progress: 0,
-    label: '100%',
-  },
-];
 
 function App() {
   return (
---
+++ src/buttonConfigs.ts
@@ -0,0 +1,45 @@
+export const buttonConfigs = [
+  {
+    id: 0,
+    progress: 0,
+    label: '25%',
+  },
+  {
+    id: 1,
+    progress: 0,
+    label: '50%',
+  },
+  {
+    id: 2,
+    progress: 0,
+    label: '75%',
+  },
+  {
+    id: 3,
+    progress: 0,
+    label: '100%',
+  },
+];
+
+export const buttonConfigsBottom = [
+  {
+    id: 0,
+    progress: 100,
+    label: '25%',
+  },
+  {
+    id: 1,
+    progress: 0,
+    label: '50%',
+  },
+  {
+    id: 2,
+    progress: 0,
+    label: '75%',
+  },
+  {
+    id: 3,
+    progress: 0,
+    label: '100%',
+  },
+];
